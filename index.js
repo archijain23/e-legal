da5bb57b9941f6b16c1b2f80b2b32530762ece45
Archi Jain <jainarchi023@gmail.com>
2024-04-07 06:57:56 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express= require("express");
 const app= express();
 const path=require("path");
+const server=require("http").createServer(app);
+
+const io= require("socket.io")(server);
 
 app.set("view engine", "ejs");
 app.set("views",path.join(__dirname,"/views"));
 app.use(express.static(path.join(__dirname , "public")));
 
 app.use(express.urlencoded({extended:true}));
+
+io.on("connection", function(socket){
+    socket.on("newuser",function(username){
+        socket.broadcast.emit("update",username+"joined the conversation");
+    })
+    socket.on("exituser",function(username){
+        socket.broadcast.emit("update",username+"left the conversation");
+    })
+    socket.on("chat",function(message){
+        socket.broadcast.emit("update",username+"left the conversation");
+    })
+})
 
 const { v4: uuidv4 } = require('uuid');
 
@@ -48,4 +63,8 @@ app.get("/about",(req,res) =>{
 
 app.get("/signin-advisor" ,(req,res) =>{
     res.render("sign.ejs")
+})
+
+app.get("/ask-anon/chat",(req,res) =>{
+    res.render("chat.ejs")
 })
